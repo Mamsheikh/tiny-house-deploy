fd44cbfeb7129f2c4b6a6de62027f758e54399de
unknown <mamsheikh123@gmail.com>
2021-06-23 14:59:42 -0700

    first commit

---
+++ src/lib/types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ListingType = void 0;
+var ListingType;
+(function (ListingType) {
+    ListingType["Apartment"] = "APARTMENT";
+    ListingType["House"] = "HOUSE";
+})(ListingType = exports.ListingType || (exports.ListingType = {}));
+//# sourceMappingURL=types.js.map
