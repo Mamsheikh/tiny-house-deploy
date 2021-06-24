811f84f066229c664f061e61b8abd2932e6f566e
unknown <mamsheikh123@gmail.com>
2021-06-24 13:21:20 -0700

    deploy

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const compression_1 = __importDefault(require("compression"));
