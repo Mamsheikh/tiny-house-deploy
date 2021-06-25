1390207ed1187e29c309efd9e0835db7c68fa8ab
Mamsheikh <36868017+Mamsheikh@users.noreply.github.com>
2021-06-25 03:43:28 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const mount = (app) => __awaiter(void 0, void 0, void 0, function* () {
     app.use(cookie_parser_1.default(process.env.SECRET));
     app.use(compression_1.default(), express_1.default.json({ limit: "2mb" }), express_1.default.static(path_1.default.join(__dirname, 'client')), cookie_parser_1.default(process.env.SECRET), cors_1.default());
     app.get("/*", (_req, res) => {
-        res.sendFile(path_1.default.join(__dirname, 'client', 'client/index.html'));
+        res.sendFile(path_1.default.join(__dirname, 'client', 'index.html'));
     });
     const server = new apollo_server_express_1.ApolloServer({
         typeDefs: graphql_1.typeDefs, resolvers: graphql_1.resolvers, context: ({ req, res }) => ({ db, req, res })
